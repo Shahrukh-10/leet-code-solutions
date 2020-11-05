2b0b9570f70cb2e8730b3c643167684079146d7c
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2020-11-05 23:45:03 +0530

    Create Roman_to_integer.java
    https://leetcode.com/problems/roman-to-integer/

---
+++ Roman to Integer/Roman_to_integer.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+class Solution {
+    public int romanToInt(String s) {
+        Map<Character,Integer> map = new HashMap<>();
+        map.put('I', 1);
+        map.put('V', 5);
+        map.put('X', 10);
+        map.put('L', 50);
+        map.put('C', 100);
+        map.put('D', 500);
+        map.put('M', 1000);
+        
+        char[] arr=s.toCharArray();
+        int sum=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            if(i>0 && (map.get(arr[i])>map.get(arr[i-1])))
+               {
+                   sum=sum+(map.get(arr[i])-map.get(arr[i-1]))-map.get(arr[i-1]);
+               }
+            else
+               {
+                   sum=sum+map.get(arr[i]);
+               }
+         }
+                   return sum;
+
+    }
+    public static void main(String[] args)
+    {
+      String s="III";
+      System.out.print(romanToInt(s));
+    }
+    
+    
+    }
