1342e9c790f4944a8dacdb81c7afab30f36fa515
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2020-11-10 13:22:57 +0530

    https://leetcode.com/problems/reverse-integer

--- Reverse Integer/reverse_integer.java
+++ Reverse Integer/reverse_integer.java
@@ -1,27 +1,50 @@
 public class HelloWorld{
+                static int reverse(int x) {
+                        String s=Integer.toString(x);
+                        StringBuilder sb= new StringBuilder();
+                        sb.append(s);
+                        sb=sb.reverse();
+                        String a=sb.toString();
+
+                        if(a.contains("-"))
+                        {
+                            a=a.replace("-","");
+                            a="-".concat(a);
+                             Double d = Double.parseDouble(a);
+                           // x=Integer.parseInt(a);
+                            if(d > 2147483647  || d < -2147483648)
+                            {
+                                return 0;
+                            }
+
+                            else
+                            {   
+                                x=Integer.parseInt(a);
+                                 return x;
+                            }
+
+                        }
 
 
-        static int reverse(int x) {
-        String s=Integer.toString(x);
-        StringBuilder sb= new StringBuilder();
-        sb.append(s);
-        sb=sb.reverse();
-        String a=sb.toString();
-        
-        if(a.contains("-"))
-        {
-            a=a.replace("-","");
-            a="-".concat(a);
-            return Integer.valueOf(a);
-        }
-        else
-        {
-            x=Integer.parseInt(a);
-            return x;
-        }
-        
-        
-        }
+                        else
+                        {
+                            Double d = Double.parseDouble(a);
+                           // x=Integer.parseInt(a);
+                            if(d > 2147483647  || d < -2147483648)
+                            {
+                                return 0;
+                            }
+
+                            else
+                            {   
+                                x=Integer.parseInt(a);
+                                 return x;
+                            }
+
+                        }
+
+
+                    }
     
      public static void main(String []args){
         
