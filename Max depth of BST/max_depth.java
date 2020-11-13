baa5d6020399a11101834124e064be082dc8501b
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2020-11-13 17:06:12 +0530

    https://leetcode.com/problems/maximum-depth-of-binary-tree/

--- Max depth of BST/max_depth.java
+++ Max depth of BST/max_depth.java
@@ -1,1 +1,24 @@
 https://leetcode.com/problems/maximum-depth-of-binary-tree/
+class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root==null)
+        {
+            return 0;
+        }
+        int l=maxDepth(root.left);
+        int r=maxDepth(root.right);
+        if(l>r)
+        {
+            return l+1;
+        }
+        else
+        {
+            return r+1;
+        }
+    }
+  public static void main(String[] args)
+  {
+    int[] root={3,9,20,null,null,15,7};
+    System.out.print(maxDepth(root));
+  }
+}
