9501d0068f00c720c39743d654b59c2fcc6dd906
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2020-11-22 21:28:10 -0800

    https://leetcode.com/problems/sqrtx

---
+++ Square root/sqrt.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int mySqrt(int x) {
+        
+        int k= (int)Math.sqrt(x);
+        return k;
+    }
+    public static void main(String[] args)
+    {
+      System.out.print(mySqrt(9));
+    }
+}
