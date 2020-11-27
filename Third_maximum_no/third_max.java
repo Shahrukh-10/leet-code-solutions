56d33c7e6244204241aea69afb1df97d4c2cf4ac
Shahrukh-10 <56799965+Shahrukh-10@users.noreply.github.com>
2020-11-27 00:24:42 -0800

    https://leetcode.com/problems/third-maximum-number

---
+++ Third_maximum_no/third_max.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+        Arrays.sort(nums);
+
+        int answer = nums[nums.length - 1], count = 1;
+
+        for (int i = nums.length - 1; i > 0; i--) {
+
+            if (nums[i] != nums[i - 1]) {
+                count++;
+            }
+
+            if (count == 3) {
+                answer = nums[i - 1];
+                break;
+            }
+        }
+        return answer;
+    }
+
+}
